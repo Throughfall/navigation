b851104f447c13fe195ab3b31f50457da1e02794
muel <muel@mkn.de>
2018-01-29 11:59:31 +0100

    init

---
+++ AndroidNavigation/Fragment2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Fragment = Android.Support.V4.App.Fragment;
+
+namespace AndroidNavigation
+{
+    public class Fragment2 : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            return inflater.Inflate(Resource.Layout.Fragment2, container, false);
+        }
+    }
+}
--- AndroidNavigation/MainActivity.cs
+++ AndroidNavigation/MainActivity.cs
@@ -1,19 +1,39 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V7.App;
 
 namespace AndroidNavigation
 {
-    [Activity(Label = "AndroidNavigation", MainLauncher = true)]
-    public class MainActivity : Activity
+    [Activity(Label = "AndroidNavigation", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
+            base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
+
+            TabLayout tabLayout = (TabLayout)FindViewById(Resource.Id.tablayout_navigation);
+
+            ViewPager viewPager = (ViewPager)FindViewById(Resource.Id.pager);
+            SetupViewPager(viewPager);
+
+            tabLayout.SetupWithViewPager(viewPager);
         }
+
+        private void SetupViewPager(ViewPager viewPager)
+        {
+            viewPager.OffscreenPageLimit = 3;
+
+            PageAdapter adapter = new PageAdapter(SupportFragmentManager);
+            adapter.AddFragment(new Fragment1(), "Read data");
+            adapter.AddFragment(new Fragment2(), "Write data");
+            adapter.AddFragment(new Fragment3(), "About the App");
+
+            viewPager.Adapter = adapter;
+        }
+
     }
 }
 
