a6fb2a06b9104875a9d2446b50f5a3d60070cbc2
muel <muel@mkn.de>
2018-01-29 11:44:23 +0100

    Projektdateien hinzufügen.

---
+++ AndroidNavigation/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidNavigation
+{
+    [Activity(Label = "AndroidNavigation", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
